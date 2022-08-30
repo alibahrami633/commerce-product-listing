badc14745c7654917becdce466c2bec30d3dd6e7
alibahrami633 <ali.bahrami633@yahoo.com>
2022-08-30 18:33:56 +1000

    fixed some small issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
+import { useEffect, useState } from "react";
 
+import products from "./assets/db/app-data.json";
 import "./styles/App.css";
+import ProductsContainer from "./components/ProductsContainer/ProductsContainer";
+import { Product } from "./common/interfaces";
 
 function App(): JSX.Element {
+  const [productsResponse, setProductsResponse] = useState<Array<Product>>();
+
+  useEffect(() => {
+    setProductsResponse(products.products); // to be replaced with API calls from services -> api
+  }, []);
+
   return (
     <div className="App">
-       This is the product page
+      {productsResponse ? (
+        <ProductsContainer products={productsResponse} />
+      ) : (
+        <div>No products found.</div>
+      )}
     </div>
   );
 }
